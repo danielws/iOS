0f405fd76a12c6c784af3a55e969269c7fa3f46e
danielws <sendto.dws@gmail.com>
2014-02-19 17:42:10 -0800

    Initial Commit

---
+++ Week2/Week2/Week2/DWSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DWSAppDelegate.h
+//  Week2
+//
+//  Created by Daniel Warner Smith on 2/19/14.
+//  Copyright (c) 2014 Daniel Warner Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DWSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
